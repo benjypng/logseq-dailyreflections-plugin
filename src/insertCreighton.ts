33dd5a5a84955a1345331c244b310c14b21d3357
hkgnp <ben@pngs.cc>
2022-06-02 14:17:47 +0800

    minor refactoring

--- src/insertCreighton.ts
+++ src/insertCreighton.ts
@@ -2,51 +2,52 @@ import { PageEntity } from "@logseq/libs/dist/LSPlugin.user";
 import { parseText } from "./utils";
 
 export async function insertCreighton(currentPage: PageEntity) {
-  // Insert block for template array to be inserted under
-  const targetBlock = await logseq.Editor.insertBlock(
-    currentPage.name,
-    `[[MornRef ☀️]]`,
-    { isPageBlock: true }
-  );
+    // Set template array to be inserted
+    const reflectionBatchBlk = [
+        { content: `What resonates with me from today's reading?` },
+        { content: `What do I have to be grateful for?` },
+        { content: 'What happened in the past day? What thoughts, words and actions did I have?' },
+        { content: 'Who do I have to pray for?' }
+    ];
 
-  // Set template array to be inserted
-  const batchBlkArr = [
-    {
-      content: `[[Creighton Daily Reflections]]
-${await parseText()}`,
-    },
-    { content: `[[Prayer list]]` },
-  ];
+    // Insert block for template array to be inserted under
+    const mornRefBlock = await logseq.Editor.insertBlock(
+        currentPage.name,
+        `[[MornRef ☀️]]`,
+        { isPageBlock: true }
+    );
 
-  // Insert template array
-  await logseq.Editor.insertBatchBlock(targetBlock.uuid, batchBlkArr, {
-    before: false,
-    sibling: false,
-  });
+    // Insert Reflections
+    const creightonBlk = await logseq.Editor.insertBlock(mornRefBlock.uuid, `[[Creighton Daily Reflections]]
+${await parseText()}`, {
+        before: false,
+        sibling: false,
+    });
 
-  // Insert empty block under reflection block
-  const reflectionBlock = await logseq.Editor.getBlock(targetBlock.uuid, {
-    includeChildren: true,
-  });
-  const targetBlock2 = await logseq.Editor.insertBlock(
-    reflectionBlock.children[0]["uuid"],
-    "{{renderer :wordcount_dailyreflections}}",
-    {
-      before: false,
-      sibling: false,
-    }
-  );
 
-  // Insert empty block
-  const wordcountBlock = await logseq.Editor.insertBlock(
-    targetBlock2.uuid,
-    "",
-    {
-      before: false,
-      sibling: false,
-    }
-  );
+    let wordCountBlk = await logseq.Editor.insertBlock(
+        creightonBlk.uuid,
+        "{{renderer :wordcount_dailyreflections}}",
+        {
+            before: false,
+            sibling: false,
+        }
+    );
 
-  // Set edit cursor to empty block
-  await logseq.Editor.editBlock(wordcountBlock.uuid);
+    await logseq.Editor.insertBatchBlock(wordCountBlk.uuid, reflectionBatchBlk, { sibling: false, before: false })
+
+    wordCountBlk = await logseq.Editor.getBlock(wordCountBlk.uuid, { includeChildren: true })
+
+    // Insert empty block
+    const editBlock = await logseq.Editor.insertBlock(
+        wordCountBlk.children[0]["uuid"],
+        "",
+        {
+            before: false,
+            sibling: false,
+        }
+    );
+
+    // Set edit cursor to empty block
+    await logseq.Editor.editBlock(editBlock.uuid);
 }
