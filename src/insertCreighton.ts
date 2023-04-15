fba9ac4b792325403b27600e8283f63129e8d73c
hkgnp <github.com/hkgnp>
2023-04-16 07:18:11 +0800

    [chore] nest reading under the creighton block

--- src/insertCreighton.ts
+++ src/insertCreighton.ts
@@ -2,44 +2,55 @@ import { BlockUUID } from "@logseq/libs/dist/LSPlugin.user";
 import { generateUniqueId, parseText } from "./utils";
 
 export async function insertCreighton(rendererBlock: BlockUUID) {
-  await logseq.Editor.updateBlock(rendererBlock, `[[MornRef ☀️]]`);
+  //await logseq.Editor.updateBlock(rendererBlock, `[[MornRef ☀️]]`);
 
   // Insert Reflections
-  const creightonBlk = await logseq.Editor.insertBlock(
+  //const creightonBlk =
+  const blk = await logseq.Editor.updateBlock(
     rendererBlock,
-    `[[Creighton Daily Reflections]]
-${await parseText()}`,
-    {
-      before: false,
-      sibling: false,
-    }
+    `[[Creighton Daily Reflections]]`
   );
 
-  const wordCountBlk = await logseq.Editor.insertBlock(
-    creightonBlk!.uuid,
-    `[[My Reflections]] {{renderer :wordcount_${generateUniqueId()}}}`,
-    {
-      before: false,
-      sibling: true,
-    }
-  );
-
-  // Insert empty block
-  const editBlock = await logseq.Editor.insertBlock(wordCountBlk!.uuid, "", {
-    before: false,
+  await logseq.Editor.insertBlock(blk.uuid, `${await parseText()}`, {
     sibling: false,
+    before: false,
   });
 
-  // Insert habit-tracker
-  await logseq.Editor.insertBlock(
-    wordCountBlk!.uuid,
-    "TODO write daily #habit-tracker",
-    {
-      before: false,
-      sibling: true,
-    }
-  );
+  //		await logseq.Editor.insertBlock(
+  //    rendererBlock,
+  //    `[[Creighton Daily Reflections]]
+  //${await parseText()}`,
+  //    {
+  //      before: false,
+  //      sibling: false,
+  //    }
+  //  );
 
-  // Set edit cursor to empty block
-  await logseq.Editor.editBlock(editBlock!.uuid);
+  //const wordCountBlk = await logseq.Editor.insertBlock(
+  //  creightonBlk!.uuid,
+  //  `[[My Reflections]] {{renderer :wordcount_${generateUniqueId()}}}`,
+  //  {
+  //    before: false,
+  //    sibling: true,
+  //  }
+  //);
+
+  //// Insert empty block
+  //const editBlock = await logseq.Editor.insertBlock(wordCountBlk!.uuid, "", {
+  //  before: false,
+  //  sibling: false,
+  //});
+
+  //// Insert habit-tracker
+  //await logseq.Editor.insertBlock(
+  //  wordCountBlk!.uuid,
+  //  "TODO write daily #habit-tracker",
+  //  {
+  //    before: false,
+  //    sibling: true,
+  //  }
+  //);
+
+  //// Set edit cursor to empty block
+  //await logseq.Editor.editBlock(editBlock!.uuid);
 }
