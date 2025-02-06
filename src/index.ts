379fc6152abe3f6e4a875d189247f61ba46e0d99
benjypng <47936793+benjypng@users.noreply.github.com>
2025-02-07 07:41:34 +0800

    feat: add toolbar

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,29 @@ import '@logseq/libs'
 
 import { handleReflections } from './handle-reflections'
 import { settings } from './settings'
+import { getDateForPageWithoutBrackets } from 'logseq-dateutils'
 
 const main = async () => {
   console.log('Creighton Daily Reflections Plugin loaded')
+
+  logseq.provideModel({
+    async insertReflection() {
+      const { preferredDateFormat } = await logseq.App.getUserConfigs()
+      const todayPageName = getDateForPageWithoutBrackets(
+        new Date(),
+        preferredDateFormat,
+      )
+      const blk = await logseq.Editor.appendBlockInPage(todayPageName, '')
+      if (blk) {
+        await handleReflections(blk.uuid)
+      }
+    },
+  })
+
+  logseq.App.registerUIItem('toolbar', {
+    key: 'logseq-dailyreflections-plugin',
+    template: `<a class="button datenlp-toolbar" data-on-click="insertReflection"><i class="ti ti-cross"></i></a>`,
+  })
 
   logseq.App.onMacroRendererSlotted(async function ({ payload }) {
     const uuid = payload.uuid
