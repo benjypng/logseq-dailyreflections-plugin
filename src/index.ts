08f88f48a4774330226856fa9095d7a381351991
benjypng <47936793+benjypng@users.noreply.github.com>
2026-01-01 13:28:39 +0800

    fix: tag and prop creation

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,22 @@ import { handleReflections } from './utils/handle-reflections'
 const main = async () => {
   logseq.UI.showMsg('logseq-dailyreflections-plugin loaded')
 
-  const gospelReflectionTag = await logseq.Editor.getTag('GospelReflection')
-  if (!gospelReflectionTag) {
-    const tag = await logseq.Editor.createTag('GospelReflection')
-    const prop = await logseq.Editor.upsertProperty('gospel-url', {
-      type: 'url',
+  const gospelUrlProp = await logseq.Editor.getPage('gospel-url')
+  if (!gospelUrlProp) {
+    const gospelReflectionTag =
+      await logseq.Editor.createTag('GospelReflection')
+    if (!gospelReflectionTag) return
+    console.log(gospelReflectionTag)
+
+    const gospelUrlProperty = await logseq.Editor.upsertProperty('gospel-url', {
+      type: 'default',
       cardinality: 'one',
-      hide: false,
-      public: false,
     })
-    const propBlock = await logseq.Editor.getBlock(prop.id)
-    if (!tag || !propBlock) return
-    await logseq.Editor.addTagProperty(tag.uuid, propBlock.uuid)
+
+    await logseq.Editor.addTagProperty(
+      gospelReflectionTag.uuid,
+      gospelUrlProperty.uuid,
+    )
   }
 
   logseq.App.onMacroRendererSlotted(async function ({ payload }) {
