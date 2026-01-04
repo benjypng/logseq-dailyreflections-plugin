b93707b527ba678ae307c19257a6e6256d441fb1
benjypng <47936793+benjypng@users.noreply.github.com>
2026-01-05 02:42:28 +0800

    fix: use user created props

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,46 @@
 import '@logseq/libs'
 
-import { PageEntity } from '@logseq/libs/dist/LSPlugin.user'
-
 import { settings } from './settings'
 import { handleReflections } from './utils/handle-reflections'
 
 const main = async () => {
   logseq.UI.showMsg('logseq-dailyreflections-plugin loaded')
 
-  // Need to wrap in a setTimeout if not there will be an error
-  setTimeout(async () => {
-    const getCreateUrlProp = async () => {
-      const page = await logseq.Editor.getPage('gospel-url')
-      if (page) return page
-      return (await logseq.Editor.upsertProperty('gospel-url', {
-        type: 'url',
-        cardinality: 'one',
-      })) as PageEntity
-    }
-    const getCreateReflectionTag = async () => {
-      const page = await logseq.Editor.getPage('GospelReflection')
-      if (page) return page
-      return await logseq.Editor.createTag('GospelReflection')
-    }
-    try {
-      const [gospelUrlProp, gospelReflectionTag] = await Promise.all([
-        getCreateUrlProp(),
-        getCreateReflectionTag(),
-      ])
-      if (!gospelUrlProp || !gospelReflectionTag) {
-        throw new Error('Entity creation failed')
-      }
-      await logseq.Editor.addTagProperty(
-        gospelReflectionTag.uuid,
-        gospelUrlProp.uuid,
-      )
-    } catch (error) {
-      console.error(error)
-      logseq.UI.showMsg(
-        'logseq-dailyreflections-plugin: Unable to create necessary tag and prop',
-        'error',
-      )
-    }
-  }, 1000)
+  //NOTE: Am removing the below as plugin created properties doesn't play nice with sync
+  //setTimeout(async () => {
+  //  const getCreateUrlProp = async () => {
+  //    const page = await logseq.Editor.getPage('gospel-url')
+  //    if (page) return page
+  //    return (await logseq.Editor.upsertProperty('gospel-url', {
+  //      type: 'url',
+  //      cardinality: 'one',
+  //    })) as PageEntity
+  //  }
+  //  const getCreateReflectionTag = async () => {
+  //    const page = await logseq.Editor.getPage('GospelReflection')
+  //    if (page) return page
+  //    return await logseq.Editor.createTag('GospelReflection')
+  //  }
+  //  try {
+  //    const [gospelUrlProp, gospelReflectionTag] = await Promise.all([
+  //      getCreateUrlProp(),
+  //      getCreateReflectionTag(),
+  //    ])
+  //    if (!gospelUrlProp || !gospelReflectionTag) {
+  //      throw new Error('Entity creation failed')
+  //    }
+  //    await logseq.Editor.addTagProperty(
+  //      gospelReflectionTag.uuid,
+  //      gospelUrlProp.uuid,
+  //    )
+  //  } catch (error) {
+  //    console.error(error)
+  //    logseq.UI.showMsg(
+  //      'logseq-dailyreflections-plugin: Unable to create necessary tag and prop',
+  //      'error',
+  //    )
+  //  }
+  //}, 1000)
 
   logseq.App.onMacroRendererSlotted(async function ({ payload }) {
     const uuid = payload.uuid
--- src/utils/write-to-logseq.ts
+++ src/utils/write-to-logseq.ts
@@ -7,15 +7,22 @@ export const writeToLogseq = async (uuid: BlockUUID, gospel: Gospel) => {
   // Update renderer block with header block
   await logseq.Editor.updateBlock(uuid, reading)
 
-  // Assign block tag
-  const gospelReflectionTag = await logseq.Editor.getTag('GospelReflection')
+  const gospelReflectionTag = await logseq.Editor.getPage('gospelreflection')
   if (!gospelReflectionTag) {
     logseq.UI.showMsg('Gospel Reflection tag not created', 'error')
     return
   }
+
+  const gospelUrlProp = await logseq.Editor.getPage('gospel-url')
+  if (!gospelUrlProp || !gospelUrlProp.ident) {
+    logseq.UI.showMsg('Gospel URL prop not created', 'error')
+    return
+  }
+  const gospelUrlPropIdent = gospelUrlProp.ident
+
   await logseq.Editor.addBlockTag(uuid, gospelReflectionTag.uuid)
 
-  await logseq.Editor.upsertBlockProperty(uuid, 'gospel-url', url)
+  await logseq.Editor.upsertBlockProperty(uuid, gospelUrlPropIdent, url)
 
   // Insert Gospel
   await logseq.Editor.insertBlock(uuid, passage, {
